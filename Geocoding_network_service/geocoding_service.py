b591e44cb467ccabbc50d63df66f9bcd63f9159d
Sindhura-b <sindhurabuggaveeti@gmail.com>
2018-03-26 15:40:55 -0400

    added fall back and comments

--- Geocoding_network_service/geocoding_service.py
+++ Geocoding_network_service/geocoding_service.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
 
 import json
@@ -10,29 +10,36 @@ import urllib.parse
 from IPython.core.debugger import Tracer
 
 
-# In[2]:
+# In[5]:
 
 
 class HttpInterface:
+    # Connect to given geocoding service
+    # Fetch data
     def get_url(self, host, path, parameters):
         URL = '{0}{1}?{2}'.format(host, path, self.encode_parameters(parameters))
         try:
             request_handler = self.execute_get(URL)
+        # Raise exception in case of an error
         except urllib.request.URLError as e:
             return False
         else:
             return request_handler
-
+    
+    # Execute get request
     def execute_get(self, url):
         return urllib.request.urlopen(url)
 
+    # Encode parameters to construct URL
     def encode_parameters(self, parameters):
         return urllib.parse.urlencode(parameters)
     
 class GetLatLong:
+    # Intialize current instance of the class
     def __init__(self, http_interface):        
         self.http_interface = http_interface
 
+    # Perform primary geocoding using google service, if it fails code falls back to here service
     def getCoords(self, address):
         google_service = GoogleService( address, self.http_interface)
         here_service = HereService(address, self.http_interface)        
@@ -42,13 +49,16 @@ class GetLatLong:
         return Coords
         
 class HereService():
+    # Host and path names of here service
     HOST = 'https://geocoder.cit.api.here.com'
     PATH = '/6.2/geocode.json'
 
+    # Intialize current instance of the class
     def __init__(self, address, http_interface):
         self.address = address
         self.http_interface = http_interface
 
+    # get longitude and latitude co-ordinates, return false if this network service fails or doesn't return output    
     def getCoords(self):
         output = self.execute_request()
         if output is False:
@@ -58,6 +68,7 @@ class HereService():
             return False        
         return coordinates
 
+    # Execute request
     def execute_request(self):
         output = self.http_interface.get_url(self.HOST, self.PATH, self.parameters())
         if output is False:
@@ -67,6 +78,7 @@ class HereService():
         else:
             return False
     
+    # Parameters required for performing here request. app_id and app_code are left open to have a working code
     def parameters(self):
         return {
             "searchtext": self.address,
@@ -74,6 +86,7 @@ class HereService():
             "app_code": "noqvr4goz4IoBVUek111aQ"
         }
 
+    # Draw out the co-odinates from the json output of here service request
     def drawCoord(self, output):
         data = json.loads(output.read().decode('utf-8'))
         if len(data['Response']['View']) is not 0:
@@ -86,13 +99,16 @@ class HereService():
             return False
 
 class GoogleService():
+    # Host and path names of here service
     HOST = 'https://maps.googleapis.com'
     PATH = '/maps/api/geocode/json'
 
+    # Intialize current instance of the class
     def __init__(self, address, http_interface):
         self.address = address
         self.http_interface = http_interface
 
+    # get longitude and latitude co-ordinates, return false if this network service fails or doesn't return output 
     def getCoords(self):
         output = self.execute_request()
         if output is False:
@@ -100,6 +116,7 @@ class GoogleService():
         coordinates = self.drawCoord(output)
         return coordinates
 
+    # Execute request
     def execute_request(self):
         output = self.http_interface.get_url(self.HOST, self.PATH, self.parameters())
         if output is False:
@@ -109,11 +126,13 @@ class GoogleService():
         else:
             return False
     
+     # Parameters required for performing here request. app_id and app_code are left open to have a working code
     def parameters(self):
         return {
             "address": self.address
         }
 
+    # Draw out the co-odinates from the json output of google service request
     def drawCoord(self, output):
         data = json.loads(output.read().decode('utf-8'))
         if len(data['results']) is not 0:
@@ -123,13 +142,11 @@ class GoogleService():
  
 
 
-# In[3]:
+# In[6]:
 
 
-http_interface = HttpInterface()
+http_interface = HttpInterface()                                    
 latlong = GetLatLong(http_interface)
-#add = "503B lonelm ct"
-#add = "kjegEHFLAHFJLHBEGRBEWH"
 add = input("Provide address of the location to geocode: ")
 coordinates = latlong.getCoords(add)
 if coordinates is False:
